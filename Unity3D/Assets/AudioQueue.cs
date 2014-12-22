6d46a5462aa0f2589e6c99d65bed095e3460fdcf
Tsarpf <tsarpf@gmail.com>
2014-12-22 16:56:21 +0200

    Addedquick and dirty unity client

---
+++ Unity3D/Assets/AudioQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+struct AudioChunk
+{
+    int count;
+}
+class AudioQueue
+{
+    int position = 0;
+    int bitDepth;
+
+    List<float> audio;
+    public AudioQueue()
+    {
+        bitDepth = (int)Math.Pow(2,16);
+        audio = new List<float>();
+    }
+
+
+
+
+    public void Enqueue(int sample)
+    {
+        float sampleFloat = sample / bitDepth * 2 - 1;
+        audio.Add(sampleFloat);
+    }
+
+    public bool Empty()
+    {
+        return position < audio.Count;
+    }
+
+    public int DataLeft()
+    {
+        return audio.Count - position;
+    }
+
+    public float Dequeue()
+    {
+        if (Empty())
+        {
+            return 0;
+        }
+        return audio[position++];
+    }
+    
+}
---
+++ Unity3D/Assets/AudioStreamStuff.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+public class AudioStreamStuff : MonoBehaviour
+{
+    public int position = 0;
+    public int sampleRate = 0;
+    public float frequency = 440;
+    AudioQueue queue;
+    StreamReader audioReader;
+    TcpClient client;
+    NetworkStream stream;
+    void Start() {
+        queue = new AudioQueue();
+        
+        client = new TcpClient("localhost", 3666);
+        stream = client.GetStream();
+
+        audioReader = new StreamReader(stream);
+        AudioClip myClip = AudioClip.Create("spotifru", 44100 * 10000, 2, 44100, false, true, GetAudioFromQueue);
+        audio.clip = myClip;
+        //AudioClip myClip = AudioClip.Create("MySinoid", 44100, 1, 44100, false, true, OnAudioRead, OnAudioSetPosition);
+        //sampleRate = AudioSettings.outputSampleRate;
+        //audio.clip = myClip;
+        audio.PlayDelayed(2); ;
+    }
+    /*
+    void OnAudioRead(float[] data) {
+        int count = 0;
+        while (count < data.Length) {
+            data[count] = Mathf.Sign(Mathf.Sin(2 * Mathf.PI * frequency * position / sampleRate));
+            position++;
+            count++;
+        }
+    }
+    */
+
+    int readPerUpdate = 44100 / 2;
+    bool start = true;
+    void Update()
+    {
+        GetAudioFromServer(readPerUpdate);
+        /*
+        if (start)
+        {
+        }
+        start = false;
+        */
+    }
+
+    void GetAudioFromServer(int sampleCount)
+    {
+        byte[] inbuffer = new byte[4];
+
+        if (stream.CanRead)
+        {
+            int i = 0;
+            while (stream.DataAvailable && i < sampleCount)
+            {
+                int bytesRead = stream.Read(inbuffer, 0, inbuffer.Length);
+                int result = System.BitConverter.ToInt32(inbuffer, 0);
+                queue.Enqueue(result);
+                i++;
+            }
+        }
+    }
+
+    void GetAudioFromQueue(float[] data)
+    {
+        if (queue.DataLeft() < data.Length)
+        {
+            Debug.Log("Warning: not enough audio, filling rest with zeroes");
+            int j = 0;
+            for (int i = 0; i < queue.DataLeft(); i++)
+            {
+                data[i] = queue.Dequeue();
+                j++;
+            }
+            for (; j < data.Length; j++)
+            {
+                data[j] = 0;
+            }
+            return;
+        }
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = queue.Dequeue();
+        }
+    }
+    void OnAudioSetPosition(int newPosition)
+    {
+        position = newPosition;
+    }
+}
